0edf6fe3539c15fb8718ffde4a53140205b356ee
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-02-28 14:49:00 -0500

    Update inference.py add docstring for tools

--- prolog_env/inference.py
+++ prolog_env/inference.py
@@ -9,12 +9,29 @@ except ImportError:
 
 
 def prolog_add_rules(id: str, code: str):
+    """Add rules to the prolog database.
+
+    Args:
+        id (str): The id of the rule.
+        code (str): The code of the rule.
+
+    Returns:
+        str: The traceback if an error occurred, otherwise None.
+    """
     try:
         janus.consult(id, code)
     except:
         return traceback.format_exc()
 
-def prolog_query(code):
+def prolog_query(code: str) -> list:
+    """Query the prolog database.
+
+    Args:
+        code (str): The code to query.
+
+    Returns:
+        list: The result of the query, or the traceback if an error occurred.
+    """
     try:
         return list(janus.query(code))
     except:
